3ebad558d2a00a62829d0c0dc7c40fa450296251
nguyendungg1103 <nguyendungg1103@gmail.com>
2022-04-21 11:07:04 +0700

    done

---
+++ bai3.js
@@ -0,0 +1,34 @@
+
+class   Quanlysach{
+    constructor(tensach,masosach,namxuatban,soquyen,tinhtrang) {
+        this.tensach = "";
+        this.masosach = number;
+        this.namxuatban = number;
+        this.soquyen = number;
+        this.tinhtrang = true;
+    }
+    getTensach(){
+        return this.tensach;
+    }
+
+    getMasosach(){
+        return this.masosach
+    }
+    setMasosach(number){
+        this.masosach = number;
+    }
+    getNamxuatban(){
+        return this.namxuatban
+    }
+    setNamxuatban(nam){
+        this.namxuatban=nam;
+    }
+    getSoquyen(){
+        return  this.getSoquyen;
+    }
+    setSoquyen(number){
+        this.soquyen = number;
+    }
+
+
+}
